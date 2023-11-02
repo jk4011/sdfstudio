645d6bc1b9000c29b6087f571b5b6c7c1f997a36
uni20181068 <jinhyuk@unist.ac.kr>
2023-11-03 03:41:21 +0900

    problem get_outputs

--- nerfstudio/models/instant_ngp.py
+++ nerfstudio/models/instant_ngp.py
@@ -170,39 +170,16 @@ class NGPModel(Model):
     def get_outputs(self, ray_bundle: RayBundle):
         assert self.field is not None
         num_rays = len(ray_bundle)
-
-        with torch.no_grad():
-            ray_samples, ray_indices = self.sampler(
-                ray_bundle=ray_bundle,
-                near_plane=self.config.near_plane,
-                far_plane=self.config.far_plane,
-                render_step_size=self.config.render_step_size,
-                cone_angle=self.config.cone_angle,
-                alpha_thre=self.config.alpha_thre,
-            )
-
-        field_outputs = self.field(ray_samples)
+        # TODO: get ray sample and indices using self.sampler
+        # TODO get field outputs using self.field
 
         # accumulation
-        packed_info = nerfacc.pack_info(ray_indices, num_rays)
-        weights = nerfacc.render_weight_from_density(
-            packed_info=packed_info,
-            sigmas=field_outputs[FieldHeadNames.DENSITY],
-            t_starts=ray_samples.frustums.starts,
-            t_ends=ray_samples.frustums.ends,
-        )
-
-        rgb = self.renderer_rgb(
-            rgb=field_outputs[FieldHeadNames.RGB],
-            weights=weights,
-            ray_indices=ray_indices,
-            num_rays=num_rays,
-        )
-        depth = self.renderer_depth(
-            weights=weights, ray_samples=ray_samples, ray_indices=ray_indices, num_rays=num_rays
-        )
-        accumulation = self.renderer_accumulation(weights=weights, ray_indices=ray_indices, num_rays=num_rays)
-        alive_ray_mask = accumulation.squeeze(-1) > 0
+        # TODO: create a packed_info tensor using nerfacc.pack_info
+        # TODO: get weights using nerfacc.render_weight_from_density
+        # TODO: get rgb using self.renderer_rgb
+        # TODO: get depth using self.renderer_depth
+        # TODO: get accumulation using self.renderer_accumulation
+        # TODO: get alive_ray_mask using accumulation
 
         outputs = {
             "rgb": rgb,
